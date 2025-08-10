76a67860da25339a0ce68366b51fa726e722b7fb
양현준 <sju09033@naver.com>
2025-08-11 02:50:40 +0900

    feat: posts 라우터와 컨트롤러 분리

--- app/src/posts/router/posts.router.js
+++ app/src/posts/router/posts.router.js
@@ -1,22 +1,22 @@
-import express from 'express'
-import commentsRouter from '../comments/router/comments.router.js'
+import express from "express";
+import commentsRouter from "../comments/router/comments.router.js";
+import {
+  getPosts,
+  getPost,
+  createPost,
+  updatePost,
+  deletePost,
+} from "../controllers/posts.controller.js"; 
 
+const router = express.Router({ mergeParams: true });
 
-const router = express.Router({ mergeParams: true })
-router.use('/:postId/comments', commentsRouter);
-router.get('/', (req, res) => {
-    res.send("hello!");
-});
-router.delete('/:postId', (req, res) => {
-    res.send("hello!");
-});
-router.put('/:postId', (req, res) => {
-    res.send("hello!");
-});
-router.post('/', (req, res) => {
-    res.send("hello!");
-});
-router.get('/:postId', (req, res) => {
-    res.send("hello!");
-});
+router.use("/:postId/comments", commentsRouter);
+
+// 라우터는 컨트롤러의 함수만 호출
+router.get("/", getPosts);
+router.post("/", createPost);
+router.get("/:postId", getPost);
+router.put("/:postId", updatePost);
+router.delete("/:postId", deletePost);
+
 export default router;
